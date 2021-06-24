7cc2dd897e5b0f6a56e0df0403828abc25f7b405
jyuly12 <yuly.gonzalez2021@gmail.com>
2021-06-24 21:52:28 +0000

    [Add] Public instance attributes

--- models/base_model.py
+++ models/base_model.py
@@ -2,6 +2,7 @@
 """Defines BaseModel class
 """
 import uuid
+import datetime
 
 
 class BaseModel:
@@ -10,10 +11,23 @@ class BaseModel:
     def __init__(self):
         """Initialize the instance attributes
         """
-        self.id = uuid.uuid4
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = self.created_at
 
     def __str__(self):
+        return ("[{}] ({}) {}".format(self.__class__, self.id, self.__dict__))
 
     def save(self):
+        """Updates the public instance attribute"""
+        self.updated_at = datetime.now()
 
     def to_dict(self):
+        """Returns a dictionary containing all keys/values of __dict__
+        of the instance
+        """
+        dictionary = self.__dict__
+        dictionary.update({"__class__": self.__class__})
+        dictionary.update({"created_at": self.created_at.isoformat()})
+        dictionary.update({"updated_at": self.updated_at.isoformat()})
+        return dictionary
