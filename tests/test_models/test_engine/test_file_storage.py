c973c551addac6ff83ec95907fd73ea255394349
Naveduran <naveduran@gmail.com>
2021-06-30 01:19:05 +0000

    [Add] basic structure of a unittest for FileStorage

---
+++ tests/test_models/test_engine/test_file_storage.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""Unit test for the module User"""
+
+import unittest
+import os
+from models import storage
+from models.base_model import BaseModel
+
+
+class Test_FileStorage(unittest.TestCase):
+    """Test for the class FileStorage"""
+
+    def test_filestorage(self):
+        """Test for the method __init__"""
+        pass
+
+    def test_all():
+        """Test for the method all"""
+        pass
+
+    def test_new():
+        """Test for the method new"""
+        pass
+
+    def test_save():
+        """Test for the method save"""
+        pass
+
+    def test_reload():
+        """Test for the method reload"""
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
