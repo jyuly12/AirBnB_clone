22fe1d457288f12bad546df0d059bcc94de6189c
Naveduran <naveduran@gmail.com>
2021-06-30 01:52:26 +0000

    [Add] basic structure of a unittest for FileStorage

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """Unit test for the module User"""
-
 import unittest
 import os
 from models import storage
@@ -12,7 +11,21 @@ class Test_FileStorage(unittest.TestCase):
 
     def test_filestorage(self):
         """Test for the method __init__"""
-        pass
+        if os.path.isfile(FileStorage.__file_path):
+            os.remove(FileStorage.__file_path)
+        all_objs = storage.all()
+        # print("-- Reloaded objects --")
+        for obj_id in all_objs.keys():
+            obj = all_objs[obj_id]
+            # print(obj)
+            self.assertIsInstance(obj, BaseModel)
+
+        # print("-- Create a new object --")
+        my_model = BaseModel()
+        my_model.name = "Holberton"
+        my_model.my_number = 89
+        my_model.save()
+        # print(my_model)
 
     def test_all(self):
         """Test for the method all"""
