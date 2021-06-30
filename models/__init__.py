49de699697c9232be83beb8aa29b1db5a5e3d84f
Naveduran <naveduran@gmail.com>
2021-06-30 21:26:56 +0000

    [Update] style

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-"""Create a unique FileStorage instance
-"""
-from .engine.file_storage import FileStorage
+"""Create a unique FileStorage instance"""
+from models.engine.file_storage import FileStorage
 
 storage = FileStorage()
 storage.reload()
