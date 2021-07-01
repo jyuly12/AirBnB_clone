cb37dd0cd10bada51dfc44de6e5bd9b740fffd96
Naveduran <naveduran@gmail.com>
2021-07-01 03:24:07 +0000

    [Add] a test creating a instance from a dict

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -40,6 +40,22 @@ class TestUser(unittest.TestCase):
         self.assertIn('first_name', features.keys())
         self.assertIn('last_name', features.keys())
 
+        test_dict = {"id": "ff02d7e0-4254-43b3-b867-d9decb0dda13",
+                     "created_at": "2021-06-28T17:47:38.773238",
+                     "updated_at": "2021-06-28T17:47:38.773248",
+                     "__class__": "User",
+                     "first_name": "Pepito",
+                     "last_name": "Perez",
+                     "password": "123456",
+                     "email": "dalisaurio@perrito.com"}
+        instance2 = User(**test_dict)
+        self.assertTrue(isinstance(test, User))
+        self.assertEqual(instance2.id, "ff02d7e0-4254-43b3-b867-d9decb0dda13")
+        self.assertEqual(instance2.first_name, "Pepito")
+        self.assertEqual(instance2.last_name, "Perez")
+        self.assertEqual(instance2.email, "dalisaurio@perrito.com")
+        self.assertEqual(instance2.password, "123456")
+
     def test_userstr(self):
         """Test for the method __str__"""
         cs = '[User] ({}) {}'.format(self.instance.id, self.instance.__dict__)
@@ -58,6 +74,7 @@ class TestUser(unittest.TestCase):
         type_of_dict = str(type(self.instance.to_dict()))
         self.assertEqual(type_of_dict, "<class 'dict'>")
         self.assertIn(self.instance_name, self.data_base.keys())
+
 
 if __name__ == '__main__':
     unittest.main()
