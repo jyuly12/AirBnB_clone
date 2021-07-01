43e4a6dfb28ba97b451e7d2156ee87ea97360347
jyuly12 <78006686+jyuly12@users.noreply.github.com>
2021-06-30 23:07:22 -0500

    [Update] do_count condition

--- console.py
+++ console.py
@@ -86,15 +86,16 @@ class HBNBCommand(cmd.Cmd):
         args = arg.split()
         counter = 0
         objects = storage.all()
-        for key in objects.keys():
-            value = objects.get(key)
-            if args[0] in self.classes:
-                # if we have created that kind of objects
-                if value.__class__.__name__ == args[0]:
-                    # if the class of the object matches
-                    # the one they ask us
-                    counter += 1
-        print(counter)
+        if arg != None:
+            for key in objects.keys():
+                value = objects.get(key)
+                if args[0] in self.classes:
+                    # if we have created that kind of objects
+                    if value.__class__.__name__ == args[0]:
+                        # if the class of the object matches
+                        # the one they ask us
+                        counter += 1
+            print(counter)
 
     def do_create(self, arg):
         """Creates a new instance of BaseModel, saves it (to the JSON file) and
